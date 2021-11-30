cd4d34d86791b212a11b68de51da04bc7abbdaf6
ergha.dps <91133873+erghadps@users.noreply.github.com>
2021-11-30 19:16:08 +0700

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
+
+export {
+  Header, Menu, Dashboard, Footer,
+}
